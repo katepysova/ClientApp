b7b3b5cb6a4c8fec9b4180bbbf00102800aa4a0f
Kateryna Pysova <katepysova97@gmail.com>
2024-07-17 13:31:49 +0200

    layout update

--- src/components/HomePageComponents/Home.jsx
+++ src/components/HomePageComponents/Home.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import "./Home.scss";
 
 export default function Home() {
-  return <div className={"test"}>Home</div>;
+  return (
+    <div className="home-page">
+      <div className="home-page__container container">Home Page</div>
+    </div>
+  );
 }
