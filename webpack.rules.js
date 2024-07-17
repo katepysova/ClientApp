dcb9b47262fdb33cd499de824489af920621d3ba
Kateryna Pysova <katepysova97@gmail.com>
2024-07-17 11:19:12 +0200

    .prettier.json and .stylelintrc.json add

--- webpack.rules.js
+++ webpack.rules.js
@@ -1,8 +1,5 @@
 module.exports = [
-  // Add support for native node modules
   {
-    // We're specifying native_modules in the test because the asset relocator loader generates a
-    // "fake" .node file which is really a cjs file.
     test: /native_modules[/\\].+\.node$/,
     use: "node-loader",
   },
@@ -17,6 +14,10 @@ module.exports = [
     },
   },
   {
+    test: /\.css$/,
+    use: [{ loader: "style-loader" }, { loader: "css-loader" }],
+  },
+  {
     test: /\.jsx?$/,
     use: {
       loader: "babel-loader",
@@ -29,11 +30,8 @@ module.exports = [
   {
     test: /\.s[ac]ss$/i,
     use: [
-      // Creates `style` nodes from JS strings
       "style-loader",
-      // Translates CSS into CommonJS
       "css-loader",
-      // Compiles Sass to CSS
       "sass-loader",
     ],
   },
