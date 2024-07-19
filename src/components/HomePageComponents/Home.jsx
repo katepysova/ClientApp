038d7c58e1a90818d5e20ccb190961c2393b9d7b
Malte Heuser <malte.heuser@ing.com>
2024-07-19 12:04:39 +0200

    Taking absolute MacOS DB path [temporary solution]

--- src/components/HomePageComponents/Home.jsx
+++ src/components/HomePageComponents/Home.jsx
@@ -2,33 +2,100 @@ import React, { useEffect, useState } from "react";
 import "./Home.scss";
 
 export default function Home() {
-  const [data, setData] = useState([]);
-
-  const getData = async () => {
-    try {
-      const data = await window.electron.fetchData("SELECT * FROM TaskConsumption");
-      setData(data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  const [intervals, setIntervals] = useState([]);
+  const [tasks, setTasks] = useState([]);
+  const [taskConsumptions, setTaskConsumptions] = useState([]);
 
   useEffect(() => {
-    getData();
+    const fetchData = async () => {
+      const intervalData = await window.electron.fetchData("SELECT * FROM Interval");
+      setIntervals(intervalData);
+
+      const taskData = await window.electron.fetchData("SELECT * FROM Task");
+      setTasks(taskData);
+
+      const taskConsumptionData = await window.electron.fetchData("SELECT * FROM TaskConsumption");
+      setTaskConsumptions(taskConsumptionData);
+    };
+
+    fetchData();
   }, []);
 
+  const renderIcon = (iconData) => {
+    if (iconData && iconData.length > 0) {
+      const base64Icon = btoa(String.fromCharCode.apply(null, new Uint8Array(iconData)));
+      return <img src={`data:image/png;base64,${base64Icon}`} alt="Task Icon" />;
+    }
+    return null;
+  };
+
   return (
-    <div className="home-page">
-      <div className="home-page__container container">
-        <div>
-          <h2>Hello</h2>
-          <ul>
-            {data.map((row, index) => (
-              <li key={index}>{JSON.stringify(row)}</li>
-            ))}
-          </ul>
-        </div>
-      </div>
+    <div>
+      <h2>Intervals</h2>
+      <table className="data-table">
+        <thead>
+        <tr>
+          <th>ID</th>
+          <th>PC ID</th>
+          <th>Start Time</th>
+          <th>Stop Time</th>
+          <th>Total Energy Consumption</th>
+          <th>Platform</th>
+        </tr>
+        </thead>
+        <tbody>
+        {intervals.map((interval) => (
+          <tr key={interval.id}>
+            <td>{interval.id}</td>
+            <td>{interval.pc_id}</td>
+            <td>{interval.start_time}</td>
+            <td>{interval.stop_time}</td>
+            <td>{interval.total_energy_consumption}</td>
+            <td>{interval.platform}</td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
+
+      <h2>Tasks</h2>
+      <table className="data-table">
+        <thead>
+        <tr>
+          <th>Task Name</th>
+          <th>Icon</th>
+        </tr>
+        </thead>
+        <tbody>
+        {tasks.map((task) => (
+          <tr key={task.task_name}>
+            <td>{task.task_name}</td>
+            <td>{renderIcon(task.icon)}</td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
+
+      <h2>Task Consumptions</h2>
+      <table className="data-table">
+        <thead>
+        <tr>
+          <th>ID</th>
+          <th>Task Name</th>
+          <th>Interval ID</th>
+          <th>Energy Consumption</th>
+        </tr>
+        </thead>
+        <tbody>
+        {taskConsumptions.map((consumption) => (
+          <tr key={consumption.id}>
+            <td>{consumption.id}</td>
+            <td>{consumption.task_name}</td>
+            <td>{consumption.interval_id}</td>
+            <td>{consumption.energy_consumption}</td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
     </div>
   );
 }
