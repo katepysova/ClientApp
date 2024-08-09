22394e487900d7afed0c2312c5eae7bbdb73ea55
Joseph <lijihao1997@126.com>
2024-08-09 18:01:43 +0800

    build statics

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   /**
    * This is the main entry point for your application, it's the first file
@@ -5,7 +7,17 @@ module.exports = {
    */
   entry: "./src/main.js",
   // Put your normal webpack config below here
+  output: {
+    path: path.resolve(__dirname, ".webpack/main"),
+    filename: "index.js"
+  },
   module: {
     rules: require("./webpack.rules")
-  }
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    extensions: [".js", ".jsx", ".json"]
+  },
+  target: "electron-main",
+  mode: "development",
 };
--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,6 +1,14 @@
 const rules = require("./webpack.rules");
+const path = require("path");
+
 module.exports = {
   module: {
     rules
-  }
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+    alias: {
+      '@': path.resolve(__dirname, 'src'), 
+    }
+  },
 };
